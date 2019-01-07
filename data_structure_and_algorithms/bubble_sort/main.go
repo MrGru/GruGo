c75c7eeec869726f440a2896a3364b7be2ea6117
MrGru <duan.hitex@gmail.com>
2019-01-07 10:10:50 +0700

    data structure and algorithms - bubble sort

---
+++ data_structure_and_algorithms/bubble_sort/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateSlice(size int) []int {
+	slice := make([]int, size, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(999) - rand.Intn(999)
+	}
+	return slice
+}
+
+func bubbleSort(items []int) {
+	var (
+		n      = len(items)
+		sorted = false
+	)
+	for !sorted {
+		swapped := false
+		for i := 0; i < n-1; i++ {
+			if items[i] > items[i+1] {
+				items[i+1], items[i] = items[i], items[i+1]
+				swapped = true
+			}
+		}
+		if !swapped {
+			sorted = true
+		}
+		n = n - 1
+	}
+}
+func main() {
+	slice := generateSlice(20)
+	fmt.Println("\n--- Unsorted ---\n\n", slice)
+	bubbleSort(slice)
+	fmt.Println("\n--- Sorted ---\n\n", slice, "\n")
+}
